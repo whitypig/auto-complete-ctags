e4a1cc30539097b18f849683cdf579008104980a
whitypig <whitypig@gmail.com>
2011-09-10 19:13:27 +0900

    Implemented ac-ctags-is-valid-tags-file-p. Added some function definitions for the present.

--- auto-complete-ctags.el
+++ auto-complete-ctags.el
@@ -47,7 +47,30 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun ac-ctags-visit-tags-file ()
   "Select tags file."
-  (interactive))
+  (interactive)
+  (let ((tagsfile (read-file-name "Visit tags file (default tags): "
+                                   nil
+                                   "tags"
+                                   t)))
+    (when (ac-ctags-is-valid-tags-file-p tagsfile)
+      ;; ask user whether the tags will be inserted into the current
+      ;; list or new one.
+      (if (ac-ctags-create-new-list-p)
+          (ac-ctags-insert-into-new-list tagsfile)
+        (ac-ctags-insert-into-current-list tagsfile)))
+    ;; Either way, we have to (re)build completion table.
+    (ac-ctags-build-completion-table)))
+
+(defun ac-ctags-create-new-list-p (tagsfile)
+  "Ask user whether to create the new tags file list or use the
+  current one."
+  )
+
+(defun ac-ctags-insert-into-new-list (tagsfile)
+  "")
+
+(defun ac-ctags-insert-into-current-list (tagsfile)
+  "")
 
 (defun ac-ctags-select-tags-list ()
   "Swith to another list of tags."
@@ -56,12 +79,22 @@
 (defun ac-ctags-is-valid-tags-file-p (tags)
   "Return t if TAGS is valid tags file created by exuberant
   ctags."
-  nil)
+  (let ((fullpath (and (file-exists-p tags) (expand-file-name tags)))
+        (needle "!_TAG_PROGRAM_NAME	Exuberant Ctags"))
+    (when fullpath
+      (with-temp-buffer
+        ;; So far we think 500 is enough.
+        (insert-file-contents-literally fullpath nil 0 500)
+        (search-forward needle nil t)))))
 
 (defun ac-ctags-build-completion-table (tagslist)
-  "Build completion table from TAGSLIST.")
+  "Build completion table from TAGSLIST."
+  (loop for t in tagsfile
+        collect (ac-ctags-build-completion-table-from-tags t)))
 
 (defun ac-ctags-build-completion-table-from-tags (tags)
+  "Extract tag information for each entry in TAGS and return them
+  as a list."
   )
 
 (provide 'auto-complete-ctags)
