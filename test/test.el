ccd4fb8415bb046bca25b04d4ea74cebad955f65
whitypig <whitypig@gmail.com>
2011-09-10 18:40:24 +0900

    Added test for ac-ctags-is-valid-tags-file-p.

---
+++ test/test.el
@@ -0,0 +1,12 @@
+(require 'ert)
+
+(ert-deftest test-ac-ctags-is-valid-tags-file-p ()
+  "A test to check whether a tags file is created by Exuberant
+ctags."
+  (let ((tags (and (cd "~/repos/git_repos/auto-complete-ctags/test/")
+                   "./test.tags"))
+        (nonexist "./tags"))
+    (should (equal t (not (null (ac-ctags-is-valid-tags-file-p tags)))))
+    (should (equal t (null (ac-ctags-is-valid-tags-file-p nonexist))))
+    ;; check for TAGS created by etags.
+    (should (equal t (null (ac-ctags-is-valid-tags-file-p "qt.TAGS"))))))
